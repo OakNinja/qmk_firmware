141dcff04f492aff545045ddf4fe4ff4e7d76530
Esse Woods <oakninja@gmail.com>
2022-04-11 22:57:12 +0200

    Kyria with bongo cat

---
+++ keyboards/splitkb/kyria/keymaps/oakninja/rules.mk
@@ -0,0 +1,22 @@
+OLED_ENABLE = yes
+OLED_DRIVER = SSD1306   # Enables the use of OLED displays
+ENCODER_ENABLE = yes       # Enables the use of one or more encoders
+RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
+
+# https://docs.qmk.fm/#/squeezing_avr
+CONSOLE_ENABLE = no
+COMMAND_ENABLE = no
+MOUSEKEY_ENABLE = no
+LTO_ENABLE = yes
+SPACE_CADET_ENABLE = no
+GRAVE_ESC_ENABLE = no
+MAGIC_ENABLE = no
+
+WPM_ENABLE = yes
+EXTRAKEY_ENABLE = yes  # Media
+
+
+
+QMK_LOGO_ENABLE = no
+KYRIA_LOGO_ENABLE = no
+
